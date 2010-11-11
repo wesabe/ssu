96b5e455927634f45d41005071cf63026d6e7d9a
Brian Donovan <me@brian-donovan.com>
2010-11-11 09:39:25 -0800

    Add OFX info for Discover Card.

---
+++ application/chrome/content/wesabe/fi-scripts/us-002551.js
@@ -0,0 +1,10 @@
+wesabe.download.OFXPlayer.register({
+  fid: 'us-002551',
+  org: 'Discover Card',
+
+  fi: {
+    ofxFid: '7101',
+    ofxOrg: 'Discover Financial Services',
+    ofxUrl: 'https://ofx.discovercard.com',
+  },
+});
