b949f6752a4baa59eb1fd6c136acd4ea52fef608
Brian Donovan <me@brian-donovan.com>
2010-11-12 17:50:24 -0800

    Add OFX info for USAA.

---
+++ application/chrome/content/wesabe/fi-scripts/us-000840.js
@@ -0,0 +1,10 @@
+wesabe.download.OFXPlayer.register({
+  fid: 'us-000840',
+  org: 'USAA Federal Savings Bank',
+
+  fi: {
+    ofxFid: "24591",
+    ofxOrg: "USAA",
+    ofxUrl: "https://service2.usaa.com/ofx/OFXServlet",
+  },
+});
