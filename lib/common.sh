08e65adf234bd9c2dc4944a6baf935e7e37796f8
Brian Donovan <me@brian-donovan.com>
2011-05-18 21:07:12 -0700

    Extract some common functionality in the bash scripts.

---
+++ lib/common.sh
@@ -0,0 +1,42 @@
+config()
+{
+  section=$1
+  key=$2
+
+  foundsection=false
+
+  while read line
+  do
+    if [[ "$foundsection" == true ]]; then
+      if [[ "$line" =~ "[*" ]]; then
+        break
+      elif [[ "$line" =~ "$key="* ]]; then
+        echo "${line:$(expr ${#key}+1)}" | tr -d "\r\n"
+        break
+      fi
+    elif [[ "$line" == "[$section]"* ]]; then
+      foundsection=true
+    fi
+  done < application/application.ini
+}
+
+info()
+{
+  echo -e "\033[36m~> $1\033[0m"
+}
+
+yay()
+{
+  echo -e "\033[32m~> $1\033[0m"
+}
+
+warn()
+{
+  echo -e "\033[33m~> $1\033[0m"
+}
+
+die()
+{
+  echo -e "\033[31m~> $1\033[0m"
+  exit 1
+}
