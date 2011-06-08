8b1f0c1fa75b56a61d8564bc190b2ce8ec02bf52
Brian Donovan <me@brian-donovan.com>
2011-06-08 13:41:04 -0700

    Don't warn when viewing mixed http/https content.

--- application/defaults/preferences/prefs.js
+++ application/defaults/preferences/prefs.js
@@ -11,3 +11,4 @@ pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
+pref("security.warn_viewing_mixed", false);
